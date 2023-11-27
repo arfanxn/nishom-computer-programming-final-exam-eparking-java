b41c2c0a4b41ee7b19f60a1799f939afc13998a6
arfanxn <arfan2173@gmail.com>
2023-11-28 00:23:05 +0700

    add to remove on next commit

--- src/main/java/Controllers/ParkedVehicleController.java
+++ src/main/java/Controllers/ParkedVehicleController.java
@@ -4,10 +4,37 @@
  */
 package Controllers;
 
+import Interfaces.Model;
+import Interfaces.Repository;
+import Interfaces.View;
+import Models.ParkedVehicle;
+import Repositories.ParkedVehicleRepository;
+import Views.ParkedVehicleView;
+
 /**
  *
  * @author arfanxn
  */
-public class ParkedVehicleController {
+public class ParkedVehicleController extends Controller<Model, Repository, View> {
+
+    public ParkedVehicleController(Model model, Repository repository, View view) {
+        super(model, repository, view);
+    }
     
+    /**
+     *
+     */
+    public ParkedVehicleController() {
+        ParkedVehicle model = new ParkedVehicle();
+        ParkedVehicleRepository repository = new ParkedVehicleRepository();
+        ParkedVehicleView view = new ParkedVehicleView();
+        this.setModel(model);
+        this.setRepository(repository);
+        this.setView(view);
+    }
+
+    public void index() {
+        
+    }
+
 }
