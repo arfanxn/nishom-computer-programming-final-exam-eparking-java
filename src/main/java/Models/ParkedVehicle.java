a42a4625e2788ce9404928e053393a4ce3f86e0f
arfanxn <arfan2173@gmail.com>
2023-12-08 15:39:52 +0700

    refactor

--- src/main/java/Models/ParkedVehicle.java
+++ src/main/java/Models/ParkedVehicle.java
@@ -4,9 +4,6 @@
  */
 package Models;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Date;
 
 /**
@@ -26,31 +23,6 @@ public class ParkedVehicle implements Interfaces.Model {
     private String vehiclePlateNumber;
     private java.util.Date enteredAt;
     private java.util.Date leftAt;
-
-    /**
-     *
-     * @param resultSet
-     * @return
-     * @throws SQLException
-     */
-    @Override
-    public ArrayList<ParkedVehicle> fromResultSet(ResultSet resultSet) throws SQLException {
-        var parkedVehicles = new ArrayList<ParkedVehicle>();
-
-        int index = 0;
-        while (resultSet.next()) {
-            ParkedVehicle parkedVehicle = new ParkedVehicle();
-            parkedVehicle.setId(resultSet.getString("id"));
-            parkedVehicle.setVehiclePlateNumber(resultSet.getString("vehicle_plate_number"));
-            parkedVehicle.setEnteredAt(resultSet.getDate("entered_at"));
-            parkedVehicle.setLeftAt(resultSet.getDate("left_at"));
-            parkedVehicles.add(index, parkedVehicle);
-
-            index++;
-        }
-
-        return parkedVehicles;
-    }
 
     @Override
     public String getTableName() {
