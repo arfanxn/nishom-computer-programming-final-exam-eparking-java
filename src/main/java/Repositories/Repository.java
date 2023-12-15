60dd56fb3bad313fa3a3d3206c799a757ce4a781
arfanxn <arfan2173@gmail.com>
2023-12-15 21:41:01 +0700

    update some setter

--- src/main/java/Repositories/Repository.java
+++ src/main/java/Repositories/Repository.java
@@ -5,14 +5,13 @@
 package Repositories;
 
 import Configs.Database;
+import Utilities.QueryBuilder;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  *
@@ -22,64 +21,20 @@ import java.util.Map;
 public class Repository {
 
     private Configs.Database databaseConfig;
-    private int limit, offset;
-    private Map<String, String> orderBys;
-    private String keyword;
-    private Connection connection;
-    private PreparedStatement preparedStatement;
-    private ResultSet resultSet;
-    private ResultSetMetaData resultSetMetaData;
-    private StringBuilder queryStringBuilder;
-    
+    protected Connection connection;
+    protected PreparedStatement preparedStatement;
+    protected ResultSet resultSet;
+    protected ResultSetMetaData resultSetMetaData;
+    protected QueryBuilder queryBuilder;
+
     public Repository(Configs.Database databaseConfig) throws SQLException {
-        this.databaseConfig = databaseConfig;
+        // by default when databaseConfig config isn't provided the repository will use the default one instead
+        this.databaseConfig = this.databaseConfig == null ? Configs.Database.getInstance() : this.databaseConfig;
         this.connection = DriverManager.getConnection(databaseConfig.getJdbcUrlString());
-        this.orderBys = new HashMap<>();
-    }
-   
-
-    public Map<String, String> getOrderBys() {
-        return orderBys;
-    }
-
-    public Repository setOrderBys(HashMap<String, String> orderBys) {
-        this.orderBys = orderBys;
-        return this;
-    }
-
-    public Repository addOrderBy(String column, String orderBy) {
-        this.orderBys = this.orderBys == null ? new HashMap<>() : this.orderBys;
-        this.orderBys.put(column, orderBy);
-        return this;
-    }
-
-    public int getLimit() {
-        return limit;
-    }
-
-    public void setLimit(int limit) {
-        this.limit = limit;
-    }
-
-    public int getOffset() {
-        return offset;
-    }
-
-    public void setOffset(int offset) {
-        this.offset = offset;
-    }
-
-    public String getKeyword() {
-        return keyword;
-    }
-
-    public void setKeyword(String keyword) {
-        this.keyword = keyword;
+        this.queryBuilder = new QueryBuilder();
     }
 
     public Database getDatabase() {
-        // by default when databaseConfig config isn't provided the repository will use the default one instead
-        this.databaseConfig = this.databaseConfig == null ? Configs.Database.getInstance() : this.databaseConfig;
         return this.databaseConfig;
     }
 
@@ -95,7 +50,7 @@ public class Repository {
     public PreparedStatement getPreparedStatement() {
         return preparedStatement;
     }
-    
+
     public void setPreparedStatement(PreparedStatement preparedStatement) {
         this.preparedStatement = preparedStatement;
     }
@@ -103,47 +58,27 @@ public class Repository {
     public ResultSet getResultSet() {
         return resultSet;
     }
-    
-    public void setResultSet(ResultSet resultSet) {
+
+    public void setResultSet(ResultSet resultSet) throws SQLException {
         this.resultSet = resultSet;
+        this.resultSetMetaData = this.resultSet.getMetaData();
     }
 
-    public ResultSetMetaData getResultSetMetaData() {
-        return resultSetMetaData;
+    public ResultSetMetaData getResultSetMetaData() throws SQLException {
+        this.resultSetMetaData = this.resultSetMetaData == null ? this.resultSet.getMetaData() : this.resultSetMetaData;
+        return this.resultSetMetaData;
     }
-    
+
     public void setResultSetMetaData(ResultSetMetaData resultSetMetaData) {
         this.resultSetMetaData = resultSetMetaData;
     }
-
-    public String getQueryString () {
-        return this.queryStringBuilder != null ? this.queryStringBuilder.toString() : "";
+    
+    public QueryBuilder getQueryBuilder() {
+        return queryBuilder;
     }
 
-    protected Repository buildSelectQueryString(String tableName) {
-        this.queryStringBuilder = new StringBuilder();
-        this.queryStringBuilder.append("SELECT * FROM");
-        this.queryStringBuilder.append(" ").append(tableName);
-        if (this.limit != 0) {
-            queryStringBuilder.append(" LIMIT ").append(Integer.toString(this.limit));
-        }
-        if (this.offset != 0) {
-            this.queryStringBuilder.append(" OFFSET ").append(Integer.toString(this.offset));
-        }
-        if (this.orderBys.isEmpty() == false) {
-            this.queryStringBuilder.append(" ORDER BY ");
-            int index = 0;
-            for (Map.Entry<String, String> set : this.orderBys.entrySet()) {
-                String column = set.getKey();
-                String orderBy = set.getValue();
-                if (index > 0) {
-                    this.queryStringBuilder.append(", ");
-                }
-                this.queryStringBuilder.append(column).append(" ").append(orderBy);
-                index++;
-            }
-        }
-        return this;
+    public void setQueryBuilder(QueryBuilder queryBuilder) {
+        this.queryBuilder = queryBuilder;
     }
 
     public Repository openConnection() throws SQLException {
