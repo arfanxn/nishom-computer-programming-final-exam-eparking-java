bcac62a96f6cd5b7c378426175bbb3a9d3213b58
arfanxn <arfan2173@gmail.com>
2023-12-15 22:55:43 +0700

    make exception class

---
+++ src/main/java/Exceptions/Validation.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exceptions;
+
+/**
+ *
+ * @author arfanxn
+ */
+public class Validation extends Exception {
+    
+    public Validation (String str) {
+        super(str);
+    }
+
+}
+
