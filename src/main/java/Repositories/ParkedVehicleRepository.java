8cd876a820901573231aea50e26b4ce8bff403b2
arfanxn <arfan2173@gmail.com>
2023-12-15 21:40:39 +0700

    update repo (still on progress )

--- src/main/java/Repositories/ParkedVehicleRepository.java
+++ src/main/java/Repositories/ParkedVehicleRepository.java
@@ -7,22 +7,24 @@ package Repositories;
 import Configs.Database;
 import java.util.List;
 import Models.ParkedVehicle;
+import java.sql.Date;
 import java.sql.SQLException;
 import java.util.ArrayList;
-
+import java.util.UUID;
 
 /**
  *
  * @author arfanxn
  */
 public class ParkedVehicleRepository extends Repository {
-    
+
     private ParkedVehicle model;
     private List<ParkedVehicle> collection;
-    
+
     public ParkedVehicleRepository(Database databaseConfig) throws SQLException {
         super(databaseConfig);
         this.model = new ParkedVehicle();
+        this.queryBuilder.fillByModel(this.model);
     }
 
     public ParkedVehicle getModel() {
@@ -32,27 +34,43 @@ public class ParkedVehicleRepository extends Repository {
     public void setModel(ParkedVehicle model) {
         this.model = model;
     }
-    
+
     public List<ParkedVehicle> getCollection() {
         return collection;
     }
-    
+
     public ParkedVehicleRepository get() throws SQLException {
-        this.buildSelectQueryString(this.model.getTableName());
+        this.queryBuilder.buildSelectQuery();
+        String queryString = this.queryBuilder.getString();
 
         // Assign the values
-        this.setPreparedStatement(this.getConnection().prepareStatement(this.getQueryString()));
-        this.setResultSet(this.getPreparedStatement().executeQuery());
-        this.setResultSetMetaData(this.getResultSet().getMetaData());
+        this.preparedStatement = this.connection.prepareStatement(queryString);
+        this.resultSet = preparedStatement.executeQuery();
+        this.resultSetMetaData = this.resultSet.getMetaData();
 
-        while (this.getResultSet().next()) {
+        while (this.resultSet.next()) {
             this.collection = this.collection == null ? new ArrayList<>() : this.collection;
-            this.collection.add(model.fillByResultSet(this.getResultSet()));
+            this.collection.add(model.fillByResultSet(this.resultSet));
         }
 
         return this;
     }
 
-    
+    public ParkedVehicleRepository create(Models.ParkedVehicle model) throws SQLException {
+        this.queryBuilder.buildInsertQuery(1);
+
+        this.preparedStatement = this.connection.prepareStatement(this.queryBuilder.getString());
+        UUID uuid = UUID.randomUUID();
+        this.preparedStatement.setString(1, uuid.toString());
+        this.preparedStatement.setString(2, model.getPlateNumber());
+        this.preparedStatement.setDate(3, new Date(new java.util.Date().getTime()));
+        this.preparedStatement.setDate(4, null);
+        this.preparedStatement.setDate(5, null);
+        this.setResultSet(this.preparedStatement.executeQuery());
+
+        System.out.println(this.getResultSetMetaData());
+
+        return this;
+    }
 
 }
