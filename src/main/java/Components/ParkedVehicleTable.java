b64f092a2d2b786451c09d5b04e8b962e36b2c3e
arfanxn <arfan2173@gmail.com>
2023-12-17 17:50:45 +0700

    update table

--- src/main/java/Components/ParkedVehicleTable.java
+++ src/main/java/Components/ParkedVehicleTable.java
@@ -7,8 +7,17 @@ package Components;
 import javax.swing.JTable;
 import java.util.List;
 import Models.ParkedVehicle;
-import java.util.ArrayList;
 import javax.swing.table.DefaultTableModel;
+
+/*
+import Containers.Controllers;
+import Controllers.ParkedVehicleController;
+import Exceptions.Validation;
+import Requests.ParkedVehicle.UpdatePlateNumberRequest;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import javax.swing.event.TableModelEvent;
+ */
 
 /**
  *
@@ -19,34 +28,76 @@ public class ParkedVehicleTable extends JTable {
     private List<ParkedVehicle> rows;
 
     public ParkedVehicleTable() {
-        DefaultTableModel tableModel = (DefaultTableModel) this.getModel();
-        tableModel.setColumnIdentifiers((new ParkedVehicle().getColumnNames()));
-        this.setModel(tableModel);
-    }
-
-    public void replaceRows(List<ParkedVehicle> rows) {
-        this.removeRows();
-        this.setRows(rows);
     }
 
     public void setRows(List<ParkedVehicle> rows) {
         this.rows = rows;
-        DefaultTableModel tableModel = (DefaultTableModel) this.getModel();
-        int index = 0;
+        DefaultTableModel tableModel = new DefaultTableModel() {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                /*
+                // Make only the second columnIndex (Plate number) editable
+                return column == 1;
+                 */
+                // Make all cell uneditable
+                return false;
+            }
+        };
+        tableModel.setColumnIdentifiers((new ParkedVehicle().getColumnNames()));
         for (ParkedVehicle parkedVehicle : this.rows) {
-            tableModel.insertRow(index, parkedVehicle.getValues());
-            index++;
+            tableModel.addRow(parkedVehicle.getValues());
         }
-        this.setModel(tableModel);
-    }
+        /*
+        tableModel.addTableModelListener((TableModelEvent event) -> {
+            if (event.getType() == TableModelEvent.UPDATE) {
+                try {
+                    int rowIndex = event.getFirstRow();
+                    int columnIndex = event.getColumn();
 
-    public void removeRows() {
-        DefaultTableModel tableModel = (DefaultTableModel) this.getModel();
-        for (int i = (tableModel.getRowCount() - 1); i >= 0; i--) {
-            tableModel.removeRow(i);
+                    // Ensure the row and column are within valid bounds
+                    if (rowIndex >= 0 && rowIndex < tableModel.getRowCount() && columnIndex >= 0 && columnIndex < tableModel.getColumnCount()) {
+                    } else {
+                        return;
+                    }
+
+                    String oldPlateNumber = this.rows.get(rowIndex).getPlateNumber();
+                    String newPlateNumber = String.valueOf(tableModel.getValueAt(rowIndex, columnIndex));
+
+                    UpdatePlateNumberRequest request = new UpdatePlateNumberRequest();
+                    request.setPlateNumber(newPlateNumber);
+
+                    int option = JOptionPane.showConfirmDialog(
+                            null,
+                            "Edit " + oldPlateNumber + " to " + newPlateNumber,
+                            "Confirm edit",
+                            JOptionPane.YES_NO_OPTION,
+                            JOptionPane.QUESTION_MESSAGE
+                    );
+
+                    if (option == JOptionPane.YES_OPTION) {
+                        ParkedVehicleController pvc = Controllers.initParkedVehicle();
+                        ParkedVehicle parkedVehicle = pvc.updatePlateNumber(request);
+                        this.rows.set(rowIndex, parkedVehicle);
+                    } else if (option == JOptionPane.NO_OPTION) {
+                        tableModel.setValueAt(oldPlateNumber, rowIndex, columnIndex);
+                    }
+
+                } catch (SQLException e) {
+                    System.out.println(e);
+                } catch (Validation e) {
+                    System.out.println(e);
+                    JOptionPane.showMessageDialog(
+                            null,
+                            e.getMessage(),
+                            "Error",
+                            JOptionPane.ERROR_MESSAGE);
+                }
+            }
         }
-        this.rows = new ArrayList<>();
+        );
+         */
         this.setModel(tableModel);
+        tableModel.fireTableDataChanged();
     }
 
 }
